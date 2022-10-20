e909d1137689b4539369fa29fe99011d20495b23
juanfra312003 <80067016+juanfra312003@users.noreply.github.com>
2022-10-20 07:59:33 -0500

    feat-properties-facturas-tableView

--- src/main/java/com/abstraction/controllers/Controllers_Factura/Controller_Ver_Factura.java
+++ src/main/java/com/abstraction/controllers/Controllers_Factura/Controller_Ver_Factura.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -36,6 +37,9 @@ public class Controller_Ver_Factura {
 
     @FXML
     private Button botonCotizaciones;
+
+    @FXML
+    private TableView<?> tableViewVerFactura;
 
     @FXML
     private Button botonDashBoard;
