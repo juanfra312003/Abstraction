f0920755e073c5e021150a90139ddacea6b2c421
Diego Pardo Montero <78668255+DiegoPardoMontero@users.noreply.github.com>
2022-10-20 03:49:20 -0500

    feat-cotizacionDAO-implementation

--- src/main/java/com/abstraction/persistence/ICotizacionDAO.java
+++ src/main/java/com/abstraction/persistence/ICotizacionDAO.java
@@ -1,5 +1,17 @@
 package com.abstraction.persistence;
 
 
+import com.abstraction.entities.Cotizacion;
+import com.abstraction.entities.Pedido;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface ICotizacionDAO {
+    public boolean create(Cotizacion cotizacion);
+    public boolean edit(Long numero, Cotizacion cotizacion);
+    public boolean delete(Long numero);
+    public Cotizacion findById(Long numero);
+    public ArrayList<Cotizacion> findAll();
+    public Integer count();
 }
