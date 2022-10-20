38265ba79b87f4a51d0271049e14ba1f757ef25f
juanfra312003 <80067016+juanfra312003@users.noreply.github.com>
2022-10-20 13:18:19 -0500

    feat-vista-cotizaciones

--- src/main/java/com/abstraction/business/ICotizacion_facade.java
+++ src/main/java/com/abstraction/business/ICotizacion_facade.java
@@ -6,7 +6,7 @@ import com.abstraction.entities.CotizacionProducto;
 import java.util.ArrayList;
 
 public interface ICotizacion_facade {
-    boolean crearCotizacion(ArrayList<CotizacionProducto> prods);
+    boolean crearCotizacion(Cotizacion cotizacion);
     Cotizacion verCotizacion(Long id);
     ArrayList<Cotizacion> listarCotizaciones();
     boolean actualizarCotizacion(Cotizacion cotizacion);
