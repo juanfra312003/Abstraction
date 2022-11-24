b0d3a9cd4b938c29485b287033661814318fce26
Estebans441 <estebans441@gmail.com>
2022-11-24 12:11:22 -0500

    feat-crear-usuario

--- src/main/java/com/abstraction/controllers/Controllers_IniciarSesion/Controller_Registrar_Usuario.java
+++ src/main/java/com/abstraction/controllers/Controllers_IniciarSesion/Controller_Registrar_Usuario.java
@@ -2,14 +2,12 @@ package com.abstraction.controllers.Controllers_IniciarSesion;
 
 import com.abstraction.business.FacadeGeneral;
 import com.abstraction.business.IUsuario_facade;
+import com.abstraction.entities.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -36,22 +34,67 @@ public class Controller_Registrar_Usuario {
     private Button botonVolverIngreso;
 
     @FXML
-    private ChoiceBox<?> choiceBoxRol;
+    private ChoiceBox<String> choiceBoxRol;
 
     @FXML
     private TextField textoUsuario;
 
     public void initialize(FacadeGeneral facade){
         this.facade=(IUsuario_facade) facade;
+        choiceBoxRol.getItems().setAll("superusuario","vendedor","bodeguero","contador");
     }
 
     @FXML
     void onActionRegistrar(ActionEvent event) {
-
+        if(textoUsuario.getText().isBlank()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Datos incorrectos");
+            alert.setTitle("Error creando el usuario");
+            alert.setContentText("El campo de usuario no puede estar vacio");
+            alert.show();
+            textoUsuario.clear();
+        }
+        else if (PasswordField.getText().isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Datos incorrectos");
+            alert.setTitle("Error creando el usuario");
+            alert.setContentText("El campo de contrasena no puede estar vacio");
+            alert.show();
+            PasswordField.clear();
+        }
+        else if(choiceBoxRol.getValue() == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Datos incorrectos");
+            alert.setTitle("Error creando el usuario");
+            alert.setContentText("Elija un rol para el usuario");
+            alert.show();
+            textoUsuario.clear();
+        }
+        else{
+            if(facade.create(new Usuario(textoUsuario.getText(), PasswordField.getText(), choiceBoxRol.getValue()))) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setHeaderText("Usuario creado");
+                alert.setTitle("Usuario creado correctamente");
+                alert.setContentText("El usuario ha sido creado correctamente");
+                alert.showAndWait();
+                cargarIngreso();
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("Error");
+                alert.setTitle("Error creando el usuario");
+                alert.setContentText("Error desconocido");
+                alert.show();
+                textoUsuario.clear();
+            }
+        }
     }
 
     @FXML
     void onActionVolverIngreso(ActionEvent event) {
+        cargarIngreso();
+    }
+    void cargarIngreso(){
         try {
             URL fxmlLocation = getClass().getResource("/presentation/View_IniciarSesion/mockupIniciarSesion.fxml");
             System.out.println(fxmlLocation);
@@ -69,5 +112,4 @@ public class Controller_Registrar_Usuario {
             e.printStackTrace();
         }
     }
-
 }
