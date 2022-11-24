f24b1406bb75a0f7d56ffa136b1f75ea6c545e58
Estebans441 <estebans441@gmail.com>
2022-11-24 10:03:03 -0500

    feat-usuarioDAO

--- src/main/java/com/abstraction/persistence/IUsuarioDAO.java
+++ src/main/java/com/abstraction/persistence/IUsuarioDAO.java
@@ -2,5 +2,5 @@ package com.abstraction.persistence;
 import  com.abstraction.entities.Usuario;
 public interface IUsuarioDAO {
     public boolean create(Usuario usuario);
-
+    public boolean validar(Usuario usuario);
 }
